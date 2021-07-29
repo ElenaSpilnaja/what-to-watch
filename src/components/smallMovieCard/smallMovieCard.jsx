8906d9686a7f45e1d3b14ac5cdf3dee4aaefb4e1
Elena <elena.spilnaja@gmail.com>
2021-07-29 16:54:23 +0300

    add player

--- src/components/smallMovieCard/smallMovieCard.jsx
+++ src/components/smallMovieCard/smallMovieCard.jsx
@@ -1,25 +1,41 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import VideoPlayer from "../videoPlayer/videoPlayer";
 
 class SmallMovieCard extends PureComponent {
   constructor(props) {
     super(props);
+    this.state = {
+      autoplay: false,
+      controls: false,
+      muted: true,
+      video: ''
+    }
 
     this.handleChangeActive = this.handleChangeActive.bind(this);
   }
 
   handleChangeActive() {
     this.props.onCardActive(`film-id-` + this.props.film.filmId);
+    setTimeout(()=>{
+      this.setState({autoplay: true, controls: true, video: this.props.film.video})
+    },1000)
   }
 
   render() {
     const {film} = this.props;
-    const {image, title} = film;
+    const {image, title, video} = film;
     return (
       <article onMouseEnter={this.handleChangeActive} className="small-movie-card catalog__movies-card">
         <div className="small-movie-card__image">
-          <img src={image} alt={title} width="280" height="175"/>
+          <VideoPlayer
+
+                       poster={image}
+                       video={this.state.video}
+                       autoplay={this.state.autoplay}
+                       controls={this.state.controls}
+                       muted={this.state.muted}/>
         </div>
         <h3 className="small-movie-card__title">
           <Link className="small-movie-card__link" to="/films/1">{title}</Link>
---
+++ src/components/videoPlayer/videoPlayer.jsx
@@ -0,0 +1,35 @@
+﻿import React from "react";
+
+class VideoPlayer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      autoPlay: props.autoplay,
+      muted: props.muted
+    }
+
+    this.videoRef = React.createRef();
+  }
+
+  componentDidMount() {
+    const video = this.videoRef.current;
+    console.log(video)
+  }
+
+  render() {
+    const {autoPlay, muted} = this.state;
+
+    return (
+
+      <video className="player__video"
+             ref={this.videoRef}
+             poster={this.props.poster}
+             src={this.props.video}
+             autoPlay={autoPlay}
+             controls={this.props.controls} muted={muted}/>
+    );
+  }
+}
+
+export default VideoPlayer;
